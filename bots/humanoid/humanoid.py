e629d65c614a1e545cafec7d8b8c6ab312977c42
a-wilkes <a.sjwilkes@gmail.com>
2020-01-29 16:29:06 +0100

    humanoid appears to be working

--- bots/humanoid/humanoid.py
+++ bots/humanoid/humanoid.py
@@ -6,13 +6,13 @@ import random
 class Bot:
     __WIN_SCORE = 66
     __DEPTH_LIMIT = 3
-    __NUM_BELIEF_STATES = 1 # LLN
+    __NUM_BELIEF_STATES = 30 # LLN
 
     __me__ = 1
 
     __fringe = PriorityQueue()
 
-    def __init__(self, _num_beleif_states = 1, _depth_limit = 3):
+    def __init__(self, _num_beleif_states = 30, _depth_limit = 10):
         self.__DEPTH_LIMIT = _depth_limit
         self.__NUM_BELIEF_STATES = _num_beleif_states
         self.__me__ = 1
@@ -21,88 +21,65 @@ class Bot:
     ########################## heuristics ###########################
 
 
-    def action_cost(self, me, depth, state) -> float:
+    def action_cost(self, depth, curr_state) -> float:
 
         def backward_cost():
             # return (state.get_points(util.other(me)) + depth) / self.__WIN_SCORE  # the opponent's score + depth
-            return -random.random() if me == self.__me__ else random.random()
+            return -random.random() if curr_state.whose_turn() == self.__me__ else random.random()
 
         def forward_cost():
             # return (self.__WIN_SCORE - state.get_points(me)) / self.__WIN_SCORE  # my score
-            return -random.random() if me == self.__me__ else random.random()
+            return -random.random() if curr_state.whose_turn() == self.__me__ else random.random()
 
         # return backward_cost() + forward_cost()
         return (backward_cost() + forward_cost()) / 2
 
-    def midway_eval(self, depth, me, curr_state) -> float:
+    def midway_eval(self, depth, curr_state) -> float:
         # return self.bottom_decision(depth, me, curr_state)
-        return -random.random() if me == self.__me__ else random.random()
+        return -random.random() if curr_state.whose_turn() == self.__me__ else random.random()
 
-    def bottom_decision(self, me, depth, curr_state) -> float:
+    def bottom_decision(self, depth, curr_state) -> float:
         # pole = -1 if curr_state.winner()[0] != me else 1
         # return pole * (util.difference_points(curr_state, curr_state.winner()[0]) + curr_state.winner()[1]) * 1.5 * depth
-        return -random.random() if me == self.__me__ else random.random()
+        return -1.0 if curr_state.winner()[0] == self.__me__ else 1.0
 
     ########################## heuristics ###########################
 
 
-    def mind_simulation(self, me, depth, curr_state) -> float:  # using dijkstra for now
-        print("sim, ", me, ", d: ", depth)
-        print(curr_state.__repr__)
+    def mind_simulation(self, depth, curr_state) -> float:  # using dijkstra for now
         if curr_state.finished():
-            #print("win/loss: ", depth)
-            return self.bottom_decision(depth, me, curr_state)
+            return self.bottom_decision(depth, curr_state)
 
         if depth > self.__DEPTH_LIMIT:
-            #print("max depth: ", depth)
-            return self.midway_eval(depth, me, curr_state)
+            return self.midway_eval(depth, curr_state)
 
-        next_moves = curr_state.moves()
-        #print(next_moves)
+        next_moves = []
+        next_moves += curr_state.moves()
 
-        if str(type(next_moves)) == "<class 'tuple'>":
-            next_state = curr_state.clone().next(next_moves)
-            next_player = next_state.whose_turn()
-            cost = self.action_cost(next_player, depth + 1, next_state)
-            print("calc: ", cost)
-            self.__fringe.put((cost, curr_state.clone(), depth))
-        else:
-            for move in next_moves:
-                next_state = curr_state.clone().next(move)
-                next_player = next_state.whose_turn()
-                cost = self.action_cost(next_player, depth + 1, next_state)
-                print("calc: ", cost)
-                self.__fringe.put((cost, curr_state.clone(), depth))
+        for move in next_moves:
+            next_state = curr_state.clone().next(move)
+            cost = self.action_cost(depth + 1, next_state)
+            self.__fringe.put((cost, next_state.clone(), depth))
 
-        # What is choice for?
         cost, next_state, depth = self.__fringe.get()
-        print("expanding: ", cost)
 
-        next_player = next_state.whose_turn()
-        return self.mind_simulation(next_player, depth + 1, next_state)
+        return self.mind_simulation(depth + 1, next_state)
 
 
     def get_move(self, state) -> (int, int):
-        print("--------------------------------------------------------NEXT TRICK--------------------------------------------------------")
         depth = 0
         self.__me__ = state.whose_turn()
         available_moves = state.moves()
         scores = [0.0] * len(available_moves)
 
         for i, move in enumerate(available_moves):
-            print("-------------------------------------NEXT REAL MOVE-------------------------------------")
             for _ in range(self.__NUM_BELIEF_STATES):
-                print("--------------NEXT BELIEF STATE--------------")
                 next_state = self.assume_next_state(move, state)
-                scores[i] += self.mind_simulation(self.__me__, depth + 1, next_state.clone())
+                scores[i] += self.mind_simulation(depth + 1, next_state)
 
                 self.__fringe = PriorityQueue()
             scores[i] /= self.__NUM_BELIEF_STATES
-            print(scores[i])
 
-        # print(scores)
-        print("choosing: ", min(scores))
-        #print(available_moves[scores.index(max(scores))])
         return available_moves[scores.index(min(scores))]
 
 
